a5b876a3b58284932551b07c3c0204c4c5b5b5b0
vijaygopalchilkuri <vijay.gopal.c@gmail.com>
2013-06-21 15:41:44 +0200

    working prob2

--- problem2/IRPF90_temp/sum.irp.F90
+++ problem2/IRPF90_temp/sum.irp.F90
@@ -30,21 +30,21 @@ subroutine bld_sum
   character*(3) :: irp_here = 'sum'                                     ! sum.irp.f:   1
     integer :: i,j,tmp                                                  ! sum.irp.f:   8
    sum=0                                                                ! sum.irp.f:   9
-    if(MOD(term1,2).eq.0)then                                           ! sum.irp.f:  11
-        sum=sum+((term1))                                               ! sum.irp.f:  12
-    endif                                                               ! sum.irp.f:  13
-    if(MOD(term2,2).eq.0)then                                           ! sum.irp.f:  15
-        sum=sum+((term2))                                               ! sum.irp.f:  16
-    endif                                                               ! sum.irp.f:  17
-    if(MOD(term1,2).ne.0 .and. MOD(term2,2).ne.0)then                   ! sum.irp.f:  19
-        sum=sum+((term1+term2))                                         ! sum.irp.f:  20
-    endif                                                               ! sum.irp.f:  21
-    do while (term1.le.limit .or. term2.le.limit)                       ! sum.irp.f:  23
-        write(6,*)term1,term2                                           ! sum.irp.f:  25
-        term1=term1+(term2)                                             ! sum.irp.f:  27
-        term2=term2+(term1)                                             ! sum.irp.f:  28
-        if(MOD(term1,2).ne.0 .and. MOD(term2,2).ne.0)then               ! sum.irp.f:  30
-            sum=sum+((term1+term2))                                     ! sum.irp.f:  31
-        endif                                                           ! sum.irp.f:  32
-    enddo                                                               ! sum.irp.f:  35
+    do while (term1.le.limit .or. term2.le.limit)                       ! sum.irp.f:  11
+        write(6,*)term1,term2                                           ! sum.irp.f:  13
+        if(MOD(term1,2).eq.0) then                                      ! sum.irp.f:  15
+sum=sum+term1                                                           ! sum.irp.f:  15
+  endif                                                                 ! sum.irp.f:  15
+        if(MOD(term2,2).eq.0) then                                      ! sum.irp.f:  17
+sum=sum+term2                                                           ! sum.irp.f:  17
+  endif                                                                 ! sum.irp.f:  17
+        term1=term1+(term2)                                             ! sum.irp.f:  19
+        term2=term2+(term1)                                             ! sum.irp.f:  20
+    enddo                                                               ! sum.irp.f:  22
+        if(MOD(term1,2).eq.0) then                                      ! sum.irp.f:  24
+sum=sum+term1                                                           ! sum.irp.f:  24
+  endif                                                                 ! sum.irp.f:  24
+        if(MOD(term2,2).eq.0) then                                      ! sum.irp.f:  26
+sum=sum+term2                                                           ! sum.irp.f:  26
+  endif                                                                 ! sum.irp.f:  26
 end subroutine bld_sum
--- problem2/sum.irp.f
+++ problem2/sum.irp.f
@@ -8,30 +8,22 @@ BEGIN_PROVIDER [integer, sum]
     integer :: i,j,tmp
    sum=0
 
-    if(MOD(term1,2).eq.0)then
-        sum+=(term1)
-    endif
-
-    if(MOD(term2,2).eq.0)then
-        sum+=(term2)
-    endif
-
-    if(MOD(term1,2).ne.0 .and. MOD(term2,2).ne.0)then
-        sum+=(term1+term2)
-    endif
-
     do while (term1.le.limit .or. term2.le.limit)
 
         write(6,*)term1,term2
 
+        if(MOD(term1,2).eq.0)sum=sum+term1
+
+        if(MOD(term2,2).eq.0)sum=sum+term2
+
         term1+=term2
         term2+=term1
 
-        if(MOD(term1,2).ne.0 .and. MOD(term2,2).ne.0)then
-            sum+=(term1+term2)
-        endif
-
-
     enddo
+
+        if(MOD(term1,2).eq.0)sum=sum+term1
+
+        if(MOD(term2,2).eq.0)sum=sum+term2
+
 
 END_PROVIDER
